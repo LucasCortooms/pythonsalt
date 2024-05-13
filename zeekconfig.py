1a63b126578fd27b45f721b39e37f67dc049c458
Lucas <lucascortooms@hotmail.com>
2024-05-13 13:17:48 +0200

    Update zeekconfig.py

--- zeekconfig.py
+++ zeekconfig.py
@@ -2,17 +2,6 @@
 import subprocess
 import netifaces as ni
 
-def get_connection_name_from_guid(iface_guids):
-    iface_names = ['(unknown)' for _ in range(len(iface_guids))]
-    reg_key = wr.OpenKey(wr.HKEY_LOCAL_MACHINE, r'SYSTEM\\CurrentControlSet\\Control\\Network\\{4d36e972-e325-11ce-bfc1-08002be10318}')
-    for i, iface_guid in enumerate(iface_guids):
-        try:
-            reg_subkey = wr.OpenKey(reg_key, iface_guid + r'\\Connection')
-            iface_names[i] = wr.QueryValueEx(reg_subkey, 'Name')[0]
-        except FileNotFoundError:
-            pass
-    return iface_names
-
 def get_network_info():
     try:
         # Get subnet information
@@ -28,7 +17,7 @@ def get_network_info():
             modified_subnet = subnet_without_last_octet + ".0"
 
             # Get the corresponding interface name
-            interface_names = get_connection_name_from_guid(ni.interfaces())
+            interface_names = ni.interfaces()
             interface_name = interface_names[0]  # Choose the first interface (you can modify this part)
 
             zeek_networks.append(f"{modified_subnet}\t{interface_name}")
